fab4115661bb240e334bc8578e755087cbfaa737
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-09-03 23:11:40 -0400

    Created folder structure, Created LineChart, Started BarGraph

---
+++ src/components/BarChart.vue
@@ -0,0 +1,23 @@
+<script>
+import { Bar } from "vue-chartjs"
+
+export default {
+    extends: Bar,
+    data() {
+        return {
+            datacollection: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [
+                    {
+                        label: "Data One",
+                        backgroundColor: "#f25432",
+                        pointBackgroundColor: "white",
+                        borderWidth: 1,
+                        pointBorderColor: 
+                    }
+                ]
+            }
+        }
+    }
+}
+</script>
---
+++ src/views/VueChartJS.vue
@@ -0,0 +1,41 @@
+<template>
+<section class="container">
+    <h1>Demo examples of vue-chartjs</h1>
+    <div class="columns">
+        <div class="column">
+            <h3>Line Chart</h3>
+            <Line-chart></Line-chart>
+        </div>
+        <div class="column">
+            <h3>Bar Chart</h3>
+
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
+            <h3>Bubble Chart</h3>
+
+        </div>
+        <div class="column">
+            <h3>Reactivity - Live update upon change in datasets</h3>
+
+        </div>
+    </div>
+</section>
+</template>
+
+<script>
+import LineChart from "@/components/LineChart.vue"
+import BarChart from "@/components/BarChart.vue"
+import BubbleChart from "@/components/BubbleChart.vue"
+import Reactive from "@/components/Reactive.vue"
+export default {
+    name: "VueChartJS",
+    components: {
+        LineChart,
+        BarChart,
+        BubbleChart,
+        Reactive
+    }
+}
+</script>
